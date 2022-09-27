adfeaff260a19c1c9e0d502fbf8df6e006e82f4d
Josue Abotsidia <josueabots@gmail.com>
2022-09-27 13:12:46 +0000

    fixing error

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 
 def print_list_integer(my_list=[]):
 {
-for integer in my_list
-    print ("{:d}".format(integer))
+for integer in range(len(my_list)):
+    print ("{:d}".format(my_list[integer]))
 }
